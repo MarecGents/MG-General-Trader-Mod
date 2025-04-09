2dbdc4ce148af4aee9158479a841cb6238d056f4
MarecGents <marecgents_tao@sina.com>
2025-04-09 15:42:26 +0800

    fix some bugs which not filter perfectly SPT 3.11.X

--- Main/MG-GT-Mod/src/mod.ts
+++ Main/MG-GT-Mod/src/mod.ts
@@ -1,6 +1,4 @@
 import {DependencyContainer} from "tsyringe";
-import {VFS} from "@spt/utils/VFS";
-import {ILogger} from "@spt/models/spt/utils/ILogger";
 import {IPreSptLoadMod} from "@spt/models/external/IPreSptLoadMod";
 import {IPostDBLoadMod} from "@spt/models/external/IPostDBLoadMod";
 import {IPostSptLoadMod} from "@spt/models/external/IPostSptLoadMod";
@@ -11,16 +9,14 @@ import {Main} from "./custom/Main";
 export class Mod implements IPreSptLoadMod, IPostDBLoadMod, IPostSptLoadMod {
 
     public container: DependencyContainer;
-    public Logger: ILogger;
-    public VFS: VFS;
     public modpath: string;
+    public modRealpath: string;
 
     preSptLoad(container: DependencyContainer):void {
         this.container = container;
-        this.Logger = container.resolve<ILogger>("WinstonLogger");
-        this.VFS = container.resolve<VFS>("VFS");
         const PreSptModLoader:PreSptModLoader = container.resolve<PreSptModLoader>('PreSptModLoader');
         this.modpath = PreSptModLoader.getModPath("MG-GT-Mod");
+        this.modRealpath = __dirname + "\\..\\";
     }
 
     postDBLoad(container: DependencyContainer):void {
